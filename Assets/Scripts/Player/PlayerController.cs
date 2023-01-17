2a23afb108d90599922e163d884b34622f8f47f6
Samuel Cardoso <scksamuel.sc@gmail.com>
2023-01-17 00:10:00 -0300

    Implements player movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	[SerializeField] public float speed = 5;
+	private float inputX = 0;
+	private float inputY = 0;
+	private Rigidbody2D body;
+
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		CaptureInput();
+	}
+
+	void FixedUpdate() {
+		PlayerMovement();
+	}
+
+	void CaptureInput() {
+		inputX = Input.GetAxisRaw("Horizontal");
+		inputY = Input.GetAxisRaw("Vertical");
+	}
+
+	void PlayerMovement() {
+		body.velocity = new Vector2(inputX, inputY).normalized * speed;
+	}
+}
